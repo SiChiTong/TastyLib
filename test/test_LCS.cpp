cab9b8b357109bd13b88aea78d198f1b71276aa9
Steven Liu <stevennl@foxmail.com>
2017-04-03 12:25:27 +0800

    Fix warnings on g++

--- test/test_LCS.cpp
+++ test/test_LCS.cpp
@@ -7,8 +7,8 @@ using namespace tastylib;
 using std::string;
 
 TEST(LCS, Basic) {
-    ASSERT_EQ(0, lcs("", ""));
-    ASSERT_EQ(2, lcs("aa", "aa"));
-    ASSERT_EQ(7, lcs("aa123XXXXabc", "abc123abc"));
+    ASSERT_EQ((std::size_t)0, lcs("", ""));
+    ASSERT_EQ((std::size_t)2, lcs("aa", "aa"));
+    ASSERT_EQ((std::size_t)7, lcs("aa123XXXXabc", "abc123abc"));
 }
 
